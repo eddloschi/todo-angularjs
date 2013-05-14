cd9f9ed82598408800e48fb0af39450ff843248c
Maurício Batista <eddloschi@gmail.com>
2013-05-14 16:12:56 -0300

    rails_apps_composer: initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+TodoAngularjs::Application.config.secret_token = 'e0fd96bff61b101381d8906285038964089fe820a2220551ed79432e55df13e26464881cc38039e4d46cc8b34a6d85d76ffcfb16efc446d82487750dd1ae738e'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,8 @@
+# Be sure to restart your server when you modify this file.
+
+TodoAngularjs::Application.config.session_store :cookie_store, key: '_todo-angularjs_session'
+
+# Use the database for sessions instead of the cookie-based default,
+# which shouldn't be used to store highly confidential information
+# (create the session table with "rails generate session_migration")
+# TodoAngularjs::Application.config.session_store :active_record_store

cb95cc0caa864c861b2e92364403e67461141c98
Maurício Batista <eddloschi@gmail.com>
2013-05-15 15:52:04 -0300

    database seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,3 +7,9 @@
 #   Mayor.create(name: 'Emanuel', city: cities.first)
 # Environment variables (ENV['...']) are set in the file config/application.yml.
 # See http://railsapps.github.io/rails-environment-variables.html
+
+Item.create([
+  {description: 'Learn BDD', done: false},
+  {description: 'Learn Scrum', done: true},
+  {description: 'Learn Ruby', done: true}
+])
